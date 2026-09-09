3ce24600cc8230c08506711dd6159d795a33147b
Thota Ravi Kumar <145559569+kumara-ravi-27@users.noreply.github.com>
2026-09-09 17:07:12 +0530

    Refactor MainPage to include Design component

--- src/Pages/ImpPage.jsx
+++ src/Pages/ImpPage.jsx
@@ -3,29 +3,34 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
 
-
+import Design from "../details/Design";
 import NavBar from "../details/NavBar";
 import AboutMe from "../details/AboutMe";
 import Education from "../details/Education";
 import Projects from "../details/Projects";
 import Skills from "../details/Skills";
 import Contact from "../details/Contact";
-import AddInfo from "../details/AddInfo";
+import Updates from "../details/Updates";
+import Cv from "../details/Cv";
+import Privacy from "../details/Privacy";
 
 const MainPage = () => {
   return (
     <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<AboutMe />} />
-        <Route path="/Education" element={<Education />} />
-        <Route path="/Projects" element={<Projects />} />
-        <Route path="/Skills" element={<Skills />} />
-        <Route path="/AddInfo" element={<AddInfo />} />
-        <Route path="/contact" element={<Contact />} />
-        
-        
-      </Routes>
+      <Design>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<AboutMe />} />
+          <Route path="/Education" element={<Education />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Updates" element={<Updates />} />
+          <Route path="/Projects" element={<Projects />} />
+          <Route path="/Skills" element={<Skills />} />
+          <Route path="/Cv" element={<Cv />} />
+          <Route path="/Privacy" element={<Privacy />} />
+
+        </Routes>
+      </Design>
     </Router>
   );
 };
