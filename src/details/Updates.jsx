c561798f85fbedd72950cc21e19dc0ab1f22b0e8
Thota Ravi Kumar <145559569+kumara-ravi-27@users.noreply.github.com>
2026-09-09 17:21:26 +0530

    Add Updates component for professional information

---
+++ src/details/Updates.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Updates';
+
+const Updates = () => (
+  <section className="add-info">
+    <h2>Professional Update</h2>
+    <ul>
+      <li>Actively seeking new job opportunities in DEVOPS, SRE.</li>
+      
+      <li> Available to join immediately.</li>
+      
+      
+
+    </ul>
+  </section>
+);
+
+export default Updates;
